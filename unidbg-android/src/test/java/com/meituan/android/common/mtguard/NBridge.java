bf5e56a2644c41453da3d2e1c2f0af09d6f7b398
kreedz <luserkaiz@gmail.com>
2024-08-04 23:18:24 +0800

    mt init finish

--- unidbg-android/src/test/java/com/meituan/android/common/mtguard/NBridge.java
+++ unidbg-android/src/test/java/com/meituan/android/common/mtguard/NBridge.java
@@ -1,14 +1,23 @@
 package com.meituan.android.common.mtguard;
 
 import com.github.unidbg.AndroidEmulator;
+import com.github.unidbg.Emulator;
 import com.github.unidbg.Module;
 import com.github.unidbg.arm.backend.Unicorn2Factory;
+import com.github.unidbg.file.FileResult;
+import com.github.unidbg.file.IOResolver;
+import com.github.unidbg.file.linux.AndroidFileIO;
 import com.github.unidbg.linux.android.AndroidEmulatorBuilder;
 import com.github.unidbg.linux.android.AndroidResolver;
+import com.github.unidbg.linux.android.SystemPropertyHook;
+import com.github.unidbg.linux.android.SystemPropertyProvider;
 import com.github.unidbg.linux.android.dvm.*;
+import com.github.unidbg.linux.android.dvm.api.ClassLoader;
 import com.github.unidbg.linux.android.dvm.array.ArrayObject;
 import com.github.unidbg.linux.android.dvm.array.ByteArray;
 import com.github.unidbg.linux.android.dvm.wrapper.DvmInteger;
+import com.github.unidbg.linux.file.ByteArrayFileIO;
+import com.github.unidbg.linux.file.SimpleFileIO;
 import com.github.unidbg.memory.Memory;
 import com.github.unidbg.utils.Inspector;
 
@@ -20,7 +29,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NBridge extends AbstractJni {
+public class NBridge extends AbstractJni implements IOResolver {
     private final AndroidEmulator emulator;
     private final DvmClass NBridge;
     private final VM vm;
@@ -30,12 +39,49 @@ public class NBridge extends AbstractJni {
         // 注意這裡只有 32 位
         emulator = AndroidEmulatorBuilder
                 .for32Bit()
-                .addBackendFactory(new Unicorn2Factory(true))
+//                .addBackendFactory(new Unicorn2Factory(true))
                 .setProcessName("com.sankuai.meituan")
                 .build();
         Memory memory = emulator.getMemory();
+        emulator.getSyscallHandler().addIOResolver(this);
         memory.setLibraryResolver(new AndroidResolver(23));
-        vm = emulator.createDalvikVM(new File("unidbg-android/src/test/resources/apk/mt.apk"));
+
+        SystemPropertyHook systemPropertyHook = new SystemPropertyHook(emulator);
+        systemPropertyHook.setPropertyProvider(new SystemPropertyProvider() {
+            @Override
+            public String getProperty(String key) {
+                //                    case "ro.build.id":
+//                        return new StringObject(vm, "QD1A.190821.007");
+//                    case "persist.sys.usb.config":
+////                        return new StringObject(vm, "adb");
+//                        return new StringObject(vm, "charging");
+//                    case "sys.usb.config":
+//                        return new StringObject(vm, "charging");
+//                    case "sys.usb.state":
+//                        return new StringObject(vm, "charging");
+                System.out.println("kreedz getProperty: " + key);
+                switch (key){
+                    case "ro.build.user":
+                        return "user";
+                    case "ro.build.display.id":
+                        return "QD1A.190821.007";
+                    case "ro.build.host":
+                        return "abfarm799";
+//                    case "ro.serialno":
+//                        return "99211FFAZ0088R";
+//                    case "ro.product.manufacturer":
+//                        return "Google";
+//                    case "ro.product.brand":
+//                        return "google";
+//                    case "ro.product.model":
+//                        return "Pixel 4";
+                }
+                return null;
+            }
+        });
+        memory.addHookListener(systemPropertyHook);
+
+        vm = emulator.createDalvikVM(new File("unidbg-android/src/test/resources/apk/mt/mt.apk"));
         vm.setJni(this);
         vm.setVerbose(true);
         DalvikModule dm = vm.loadLibrary("mtguard", true);
@@ -44,13 +90,13 @@ public class NBridge extends AbstractJni {
         dm.callJNI_OnLoad(emulator);
 
         // start trace
-        String traceFile = "/home/kreedz/Documents/reverse/long/trace1.log";
-        try {
-            PrintStream traceStream = new PrintStream(new FileOutputStream(traceFile), true);
-            emulator.traceCode(module.base,module.base+module.size).setRedirect(traceStream);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        String traceFile = "/home/kreedz/Documents/reverse/long/trace1.log";
+//        try {
+//            PrintStream traceStream = new PrintStream(new FileOutputStream(traceFile), true);
+//            emulator.traceCode(module.base,module.base+module.size).setRedirect(traceStream);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public String mtMain(){
@@ -69,7 +115,7 @@ public class NBridge extends AbstractJni {
         vm.addLocalObject(input2_3);
         // 完整的参数2
         list.add(vm.addLocalObject(new ArrayObject(input2_1, input2_2, input2_3)));
-        Number number = module.callFunction(emulator, 0x5a38d, list.toArray());
+        Number number = module.callFunction(emulator, 0x41bd, list.toArray());
         DvmObject[] value = ((ArrayObject) vm.getObject(number.intValue())).getValue();
         StringObject result = (StringObject) value[0];
         String res = result.getValue().toString();
@@ -81,13 +127,16 @@ public class NBridge extends AbstractJni {
         List<Object> list = new ArrayList<>(10);
         list.add(vm.getJNIEnv()); // 第一个参数是env
         list.add(0); // 第二个参数，实例方法是jobject，静态方法是jclazz，直接填0，一般用不到。
-        list.add(111);
+        list.add(1);
         DvmObject<?> obj = vm.resolveClass("java/lang/object").newObject(null);
         vm.addLocalObject(obj);
         ArrayObject myobject = new ArrayObject(obj);
         vm.addLocalObject(myobject);
         list.add(vm.addLocalObject(myobject));
-        module.callFunction(emulator, 0x5a38d, list.toArray());
+        Number number = module.callFunction(emulator, 0x41bd, list.toArray());
+        ArrayObject s = vm.getObject(number.intValue());
+        String res = s.getValue()[0].getValue().toString();
+        System.out.println("init res: "+res);
     };
 
     public static void main(String[] args) {
@@ -97,5 +146,136 @@ public class NBridge extends AbstractJni {
         // 2448abf8d1cc87209245ba66fc375927
         // c21ffc63d2808d075ee18a7fd7b759ca
         // df4684330fedcd133e440c94c54e37ab
+    }
+
+    @Override
+    public DvmObject<?> callStaticObjectMethodV(BaseVM vm, DvmClass dvmClass, String signature, VaList vaList) {
+        switch (signature){
+            case "com/meituan/android/common/mtguard/NBridge->getClassLoader()Ljava/lang/ClassLoader;":
+//                return new ClassLoader(vm, signature);
+                return vm.resolveClass("java/lang/ClassLoader").newObject(null);
+            case "java/lang/Class->main2(I[Ljava/lang/Object;)Ljava/lang/Object;":
+                System.out.println("main2: "+vaList.getIntArg(0));
+//                System.out.println(vaList.getIntArg(0));
+//                return vm.resolveClass("java/lang/Object").newObject(null);
+                int arg0 = vaList.getIntArg(0);
+                switch (arg0){
+                    case 1:
+                        return new StringObject(vm, "com.sankuai.meituan");
+                    case 4:
+                        return new StringObject(vm, "ms_com.sankuai.meituan");
+                    case 5:
+                        return new StringObject(vm, "ppd_com.sankuai.meituan.xbt");
+                    case 2:
+                        return vm.resolveClass("android/app/ContextImpl").newObject(null);
+                    case 6:
+                        return new StringObject(vm, "5.1.9");
+                    case 3:
+                        return DvmInteger.valueOf(vm, 0);
+                }
+                throw new UnsupportedOperationException(signature + " -> " + arg0);
+            case "java/lang/System->getProperty(Ljava/lang/String;)Ljava/lang/String;":
+                String arg_0 = vaList.getObjectArg(0).getValue().toString();
+                switch (arg_0){
+                    case "http.proxyHost":
+                        return null;
+                    case "https.proxyHost":
+                        return null;
+                }
+                throw new UnsupportedOperationException(arg_0);
+            case "android/os/SystemProperties->get(Ljava/lang/String;)Ljava/lang/String;":
+                String argStr0 = vaList.getObjectArg(0).getValue().toString();
+                switch (argStr0){
+                    case "ro.build.id":
+                        return new StringObject(vm, "QD1A.190821.007");
+                    case "persist.sys.usb.config":
+//                        return new StringObject(vm, "adb");
+                        return new StringObject(vm, "charging");
+                    case "sys.usb.config":
+                        return new StringObject(vm, "charging");
+                    case "sys.usb.state":
+                        return new StringObject(vm, "charging");
+                }
+                throw new UnsupportedOperationException(signature + " -> " + argStr0);
+        }
+        return super.callStaticObjectMethodV(vm, dvmClass, signature, vaList);
+    }
+
+    @Override
+    public DvmObject<?> callObjectMethodV(BaseVM vm, DvmObject<?> dvmObject, String signature, VaList vaList) {
+        switch (signature){
+            case "java/lang/ClassLoader->loadClass(Ljava/lang/String;)Ljava/lang/Class;":
+                System.out.println("loadClass: " + vaList.getObjectArg(0).getValue().toString());
+                return vm.resolveClass("java/lang/Class");
+            case "android/app/ContextImpl->getPackageManager()Landroid/content/pm/PackageManager;":
+                DvmClass clazz = vm.resolveClass("android/content/pm/PackageManager");
+                return clazz.newObject(signature);
+        }
+        return super.callObjectMethodV(vm, dvmObject, signature, vaList);
+    }
+
+    @Override
+    public DvmObject<?> getObjectField(BaseVM vm, DvmObject<?> dvmObject, String signature) {
+        switch (signature){
+            case "android/content/pm/ApplicationInfo->sourceDir:Ljava/lang/String;":
+                return new StringObject(vm, "/data/app/com.sankuai.meituan-dinPQmJAjRqMIdGCOATtRg==/base.apk");
+        }
+        return super.getObjectField(vm, dvmObject, signature);
+    }
+
+    @Override
+    public DvmObject<?> getStaticObjectField(BaseVM vm, DvmClass dvmClass, String signature) {
+        switch (signature){
+            case "android/os/Build->BRAND:Ljava/lang/String;":
+                return new StringObject(vm, "google");
+            case "android/os/Build->TYPE:Ljava/lang/String;":
+                return new StringObject(vm, "user");
+            case "android/os/Build->HARDWARE:Ljava/lang/String;":
+                return new StringObject(vm, "flame");
+            case "android/os/Build->MODEL:Ljava/lang/String;":
+                return new StringObject(vm, "Pixel 4");
+            case "android/os/Build->TAGS:Ljava/lang/String;":
+                return new StringObject(vm, "release-keys");
+            case "android/os/Build$VERSION->RELEASE:Ljava/lang/String;":
+                return new StringObject(vm, "10");
+        }
+        return super.getStaticObjectField(vm, dvmClass, signature);
+    }
+
+    @Override
+    public boolean callBooleanMethodV(BaseVM vm, DvmObject<?> dvmObject, String signature, VaList vaList) {
+        switch (signature){
+            case "java/io/File->canRead()Z":
+                return true;
+        }
+        return super.callBooleanMethodV(vm, dvmObject, signature, vaList);
+    }
+
+    @Override
+    public DvmObject<?> newObjectV(BaseVM vm, DvmClass dvmClass, String signature, VaList vaList) {
+        switch (signature){
+            case "java/io/File-><init>(Ljava/lang/String;)V":
+                System.out.println("java.io.File: "+vaList.getObjectArg(0).getValue().toString());
+                return vm.resolveClass("java/io/File").newObject(vaList.getObjectArg(0).getValue().toString());
+        }
+        return super.newObjectV(vm, dvmClass, signature, vaList);
+    }
+
+    @Override
+    public FileResult resolve(Emulator emulator, String pathname, int oflags) {
+        System.out.println("kreedz path: "+pathname);
+        if (pathname.contains("/sys/class/power_supply/battery/temp")){
+            return FileResult.success(new SimpleFileIO(oflags, new File("unidbg-android/src/test/resources/apk/mt/battery/temp"), pathname));
+        }
+        if (pathname.equals("/sys/class/power_supply/battery/voltage_now")){
+            return FileResult.<AndroidFileIO>success(new ByteArrayFileIO(oflags, pathname, "4365937".getBytes()));
+        }
+//        if (pathname.contains("/data/data/com.sankuai.meituan/files/._mtg_mtdfp_up/.mini/hornCache")){
+//            return FileResult.success(new SimpleFileIO(oflags, new File("unidbg-android/src/test/resources/apk/mt/.mini/hornCache"), pathname));
+//        }
+        if (pathname.equals("/data/app/com.sankuai.meituan-dinPQmJAjRqMIdGCOATtRg==/base.apk")){
+            return FileResult.success(new SimpleFileIO(oflags, new File("unidbg-android/src/test/resources/apk/mt/mt.apk"), pathname));
+        }
+        return null;
     }
 }
